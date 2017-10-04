e7a0411a5e890dccbfc1d5ea40b97b7ef4b9cad3
mcortex <cortezmartin1983@gmail.com>
2017-10-04 00:30:45 -0300

    modificaciones minimas

--- Servo/ServoSweep/ServoSweep.ino
+++ Servo/ServoSweep/ServoSweep.ino
@@ -1,1 +1,23 @@
-/*Servo Sweep*/#include <Servo.h>Servo myServo;int posicion=0;void setup() {  myServo.attach(9);}void loop() {  for(posicion=0;posicion<=180;posicion+=1) {    myServo.write(posicion);    delay(15);  }    for(posicion=180;posicion>=0;posicion-=1) {    myServo.write(posicion);    delay(15);  }    }
+/*
+Servo Sweep
+*/
+#include <Servo.h>
+
+Servo myServo;
+int posicion=0;
+
+void setup() {
+  myServo.attach(9);
+}
+
+void loop() {
+  for(posicion=0;posicion<=180;posicion+=1) {
+    myServo.write(posicion);
+    delay(15);
+  }
+  for(posicion=180;posicion>=0;posicion-=1) {
+    myServo.write(posicion);
+    delay(15);
+  }
+    
+}
