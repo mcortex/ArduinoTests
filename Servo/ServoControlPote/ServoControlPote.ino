84942cb1de2f932ddb7b66d4e21fb8daa5159c1b
mcortex <cortezmartin1983@gmail.com>
2017-10-04 00:17:57 -0300

    Se agregan test de Servomotor

---
+++ Servo/ServoControlPote/ServoControlPote.ino
@@ -0,0 +1,1 @@
+/*Servo Control con potenciometro*/#include <Servo.h>Servo myServo;int potpin = 0;int val;void setup() {  myServo.attach(9);  }void loop() {  val = analogRead(potpin);  val = map(val,0,1023,0,180);    myServo.write(val);    delay(15);}
